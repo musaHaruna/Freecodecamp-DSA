6760cd5cbb8827927ace06baa0f1267fb8a11794
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2024-06-13 11:21:58 -0400

    reverse and find-minimum array

--- Arrays/ArrayUtil.java
+++ Arrays/ArrayUtil.java
@@ -2,7 +2,7 @@ package Arrays;
 
 
 public class ArrayUtil {
-
+ 
    public static void printArray(int[] arr) {
       int n = arr.length;
       for (int i = 0; i < n; i++) {
@@ -11,6 +11,28 @@ public class ArrayUtil {
       System.out.println();
    }
 
+  
+
+   public static void reverse(int[] numbers, int start, int end) {
+      while (start <= end) {
+          int temp = numbers[start];
+          numbers[start] = numbers[end];  
+          numbers[end] = temp;
+          start++;
+          end--;
+      }
+   }
+
+
+public static int findMinimum(int[] numbers) {
+   int min = numbers[0];
+    for (int i = 0; i < numbers.length; i++) {
+      if (numbers[i] < min) {
+        min = numbers[i];
+      }
+    }
+return min;
+}
 
    public static int[] removeEven(int[] arr) {
     int oddCount = 0;
@@ -35,8 +57,10 @@ public class ArrayUtil {
       printArray(new int[] {1, 3, 6, 8, 2, 4,7});
       int [] result = removeEven(arr);
       printArray(result);
-      
-  
+      reverse(arr, 0, arr.length - 1);
+      int minimun = findMinimum(arr);
+       System.out.println(minimun);
+    
       
    }
 
